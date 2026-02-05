ef1c5279b74bc415e03e5b620334c226afa32ea5
Da-Wei Lee <daviddwlee84@gmail.com>
2026-02-05 11:13:39 +0800

    Fix try-cli and disable toolkami by default

--- dot_config/zsh/tools/32_try.zsh
+++ dot_config/zsh/tools/32_try.zsh
@@ -1,6 +1,9 @@
-# 32_try.zsh - try-cli configuration (interactive gem tryouts)
-# https://github.com/joshmfrankel/try
+# 32_try.zsh - try-cli configuration (ephemeral workspace manager)
+# https://github.com/tobi/try
 
-command -v try &>/dev/null || return 0
+# Find try.rb from the gem - the gem wrapper is broken due to __FILE__ == $0 guard
+_try_script=$(ruby -e "require 'rubygems'; puts File.join(Gem::Specification.find_by_name('try-cli').gem_dir, 'try.rb')" 2>/dev/null)
+[[ -f "$_try_script" ]] || return 0
 
-eval "$(try init)"
+eval "$(ruby "$_try_script" init)"
+unset _try_script
